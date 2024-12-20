fix: change precision of the data when saving intrinsic parameters, solve problems when rectifying

calibrators/intrinsic_camera_calibrator/intrinsic_camera_calibrator/intrinsic_camera_calibrator/camera_models/camera_model.py
@@ -167,23 +167,23 @@ class CameraModel:
         d["camera_matrix"] = {
             "rows": 3,
             "cols": 3,
-            "data": [round(e.item(), 5) for e in self.k.flatten()],
+            "data": [round(e.item(), 9) for e in self.k.flatten()],
         }
         d["distortion_model"] = "plumb_bob"
         d["distortion_coefficients"] = {
             "rows": 1,
             "cols": 5,
-            "data": [round(e.item(), 5) for e in self.d.flatten()],
+            "data": [round(e.item(), 9) for e in self.d.flatten()],
         }
         d["projection_matrix"] = {
             "rows": 3,
             "cols": 4,
-            "data": [round(e.item(), 5) for e in p.flatten()],
+            "data": [round(e.item(), 9) for e in p.flatten()],
         }
         d["rectification_matrix"] = {
             "rows": 3,
             "cols": 3,
-            "data": [round(e.item(), 5) for e in np.eye(3).flatten()],
+            "data": [round(e.item(), 9) for e in np.eye(3).flatten()],
         }
 
         return d